3784b702b361cb1be1355f04c92b218f10b842f0
appsmall <37024455+appsmall@users.noreply.github.com>
2019-04-01 14:49:59 +0530

    - add DummyVC file
    - add DummyVC file

---
+++ DailySpend/Controller/DummyVC.swift
@@ -0,0 +1,30 @@
+//
+//  DummyVC.swift
+//  DailySpend
+//
+//  Created by apple on 01/04/19.
+//  Copyright © 2019 outect. All rights reserved.
+//
+
+import UIKit
+
+class DummyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
